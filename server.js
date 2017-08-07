210b996bfc92e32f0ae8fe374cf246e2c19ac951
gauravccloud <gaurav.sharma@cognitiveclouds.com>
2017-08-07 12:09:26 +0530

    Third Commit

--- server.js
+++ server.js
@@ -20,6 +20,12 @@ var connector = new builder.ChatConnector({
 // Listen for messages from users 
 server.post('/api/messages', connector.listen());
 
+
+server.get('/', restify.plugins.serveStatic({
+    directory: __dirname,
+    default: '/index.html'
+}));
+
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
     session.send("You said: %s", session.message.text);
